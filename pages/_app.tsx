9d503263435d87e9bb2a7c798afc4654e92a802e
Loïc Goyet - perso <loicgoyet-perso@macbook-pro-de-loic.home>
2021-06-20 19:47:09 +0200

    Install styled-components

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,25 @@
+import styled, { createGlobalStyle } from 'styled-components';
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <GlobalStyle/>
+      <Component {...pageProps} />
+    </>
+  );
 }
 export default MyApp
+
+
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    background-color: #e5e5f7;
+    opacity: 0.8;
+    background-image:  radial-gradient(#444cf7 0.5px, transparent 0.5px), radial-gradient(#444cf7 0.5px, #e5e5f7 0.5px);
+    background-size: 20px 20px;
+    background-position: 0 0,10px 10px;
+  }
+`
---
+++ pages/_document.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import Document, { DocumentContext, Head, Html, Main, NextScript } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
+
+class AppDocument extends Document {
+  // see: https://github.com/vercel/next.js/blob/master/examples/with-styled-components
+  static async getInitialProps(ctx: DocumentContext) {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: App => props => sheet.collectStyles(<App {...props} />),
+        });
+
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
+  }
+}
+
+export default AppDocument;
