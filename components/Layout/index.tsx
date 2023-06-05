564fcf958e022e2c9465ceb2f6d37b9d6bcdc217
Loïc Goyet <loic.goyet@resilience.care>
2023-06-05 18:26:12 +0200

    sort components in architecture

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import Card from '../Card';
+import Card from '../designSystem/Card';
 import React from 'react';
 import {SidePanelProvider} from './SidePanelContext';
 
